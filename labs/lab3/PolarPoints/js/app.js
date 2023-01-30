d9a303b6d346a8980422732800f809462a167d9b
vanrobb <vanrobbi@iu.edu>
2023-01-30 16:30:18 -0500

    polarpoints

--- labs/lab3/PolarPoints/js/app.js
+++ labs/lab3/PolarPoints/js/app.js
@@ -5,11 +5,12 @@ function setup(){
 }
 function draw(){
     //create a variable called res and have res call the function polarpoints
-    res=polarPoint();
+    let res=polarPoint(80);
     //translate canvas by 100 x pixels and 100 y pixels
     translate(100, 100);
     //draw a circle at res.x and res.y with a radius of 10
     circle(res.x ,res.y ,20);
+    circle(0,0,5);
 }
 //create function polarpoints with one argument r
 function polarPoint(r){
@@ -19,5 +20,5 @@ function polarPoint(r){
     //in polar points create a variable y 
     //set y to be equal to the argument r times the math cosine function with mouseX as the functions argument
     let y=r*Math.cos(mouseX);
-    createVector(x,y);
+    return createVector(x,y);
 }
