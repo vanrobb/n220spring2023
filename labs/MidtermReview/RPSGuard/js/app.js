1820ca65b2d40fecb9e4eb0f3e227f232aee3c76
vanrobb <vanrobbi@iu.edu>
2023-03-02 00:46:42 -0500

    Midterm Algorithms

---
+++ labs/MidtermReview/RPSGuard/js/app.js
@@ -0,0 +1,6 @@
+//Create variables for each of the rock, paper, scissors, guard divs
+//When one of the buttons is clicked record which one was clicked in a variable, for example if rock is clicked set variable to 1, if paper is clicked set variable to 2, etc...
+//If player chooses guard do nothing else but subtract point from player
+//Additionally run function when button is clicked that gets a random variable 1-3 for rock-scissors
+//Create Rock Paper Scissors Logic
+//If player wins there score increases, if the player loses the score decreases, if the player ties score stays the same
---
+++ labs/MidtermReview/UINav/js/app.js
@@ -0,0 +1,6 @@
+//Create Variable that represents getting class from document
+//Call upon class and set style of divs
+    //The divs should be 100px by 100px and have a gap between them
+//Create Variables for each ids
+//Make it so each div should have a different color
+//When one of the divs is clicked 
